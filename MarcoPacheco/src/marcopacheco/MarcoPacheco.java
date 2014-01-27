2cc7695c7969bb6062577a2162bf6c861dea57d6
alexky9 <ale_dya@hotmail.com>
2014-01-27 17:40:59 -0600

    Update MarcoPacheco.java

--- MarcoPacheco/src/marcopacheco/MarcoPacheco.java
+++ MarcoPacheco/src/marcopacheco/MarcoPacheco.java
@@ -1,22 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package marcopacheco;
-
-/**
- *
- * @author Internet
- */
 public class MarcoPacheco {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+       Circulo c=new Circulo();
+       System.out.println("Radio="+c.radio);
+       
+      
+    
+     if (c.radio < 0)
+      
+         System.out.println("No se permiten valores negativos");
+         
+     else
+         System.out.println("Racio ="+c.radio);
+     
+         
+         
+    
+        }
+    
     
 }
+
