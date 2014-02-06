2cda84f48ca2523df707362611712677815d97d6
Ale <Ale@Ale-PC>
2014-02-05 23:56:09 -0600

    Tarea 3

---
+++ MarcoPacheco/src/Test7/NewMain.java
@@ -0,0 +1,13 @@
+
+
+package Test7;
+
+public class NewMain {
+
+    public static void main(String[] args) {
+        Test c=new Test();
+        
+        
+    }
+    
+}
--- MarcoPacheco/src/Test7/Test.java
+++ MarcoPacheco/src/Test7/Test.java
@@ -1,15 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package Test7;
 
-/**
- *
- * @author Ale
- */
 public class Test {
+    
+    int y;
+    
+    public void test(){
+        
+        System.out.println();
+    }
     
 }
